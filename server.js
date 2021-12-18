155f6cb5f0c7c5ab4beaeff761ae029629df3429
prithivi5667 <prithivi5667@gmail.com>
2021-12-18 12:01:52 +0530

    new

--- server.js
+++ server.js
@@ -22,12 +22,8 @@ app.use(bodyParser.json());
 
 app.use(session({
     secret: process.env.SECRET,
-    saveUninitialized: true,
-resave: false,
-maxAge: 1000 * 60 * 15,
-cookie:{
-    secure: true
-       }
+    resave: false,
+    saveUninitialized: false
 }));
   
 app.use(passport.initialize());
@@ -35,7 +31,7 @@ app.use(passport.session());
 
 
 //Connecting to MongoDB using mongoose
-mongoose.connect('mongodb+srv://prithivi:prithivi@cluster0.myqm7.mongodb.net/moviesdbs?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://prithivi:prithivi@cluster0.myqm7.mongodb.net/moviesnow?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
 //Defining Schemas
@@ -144,5 +140,5 @@ router.get('/logout', function(req, res){
 
 app.use('/api', router);
 
-app.listen(process.env.PORT || 3001);
+app.listen(3001);
 console.log("Listening to port 3001\n");
